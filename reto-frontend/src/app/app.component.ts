07cfe868106013346ed9f2ef270c41876bfc727e
Juan Quintanilla <quintanilla.peru@gmail.com>
2018-09-15 12:01:22 -0500

    comit de mediodia

--- reto-frontend/src/app/app.component.ts
+++ reto-frontend/src/app/app.component.ts
@@ -1,67 +1,61 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+const misDestinos = ['Lima', 'India', 'Estados Unidos', 'USA', 'Colombia'];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  //public model: any;
   title = 'Cotizador de seguro de viajes';
   destino = '';
   timerid = 0;
   slash = '/';
+  dataDestinos = [];
   configServicios = {
     'destinos':'https://testsoat.interseguro.com.pe/talentfestapi/destinos',
+    'destinosTemp':'https://my-json-server.typicode.com/appsmambo/dataInterseguro3/destinos/?pais=',
     'cotizacion':'https://testsoat.interseguro.com.pe/talentfestapi/cotizacion'
   };
 
-  constructor(private httpClient:HttpClient){  }
-
-  buscarDestinos() {
-    console.log(this.destino);
-    //return http.get(this.configServicios.destinos).suscribe(data => {data});
-    /*return http({
-      url: this.configServicios.destinos,
-      method: 'GET',
-      data: {"destino_input": d}
-    }).then(function successCallback(response) {
-      // this callback will be called asynchronously
-      // when the response is available
-      console.log(response.data);
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      console.log(response.statusText);
-    });*/
-  }
+  constructor(private httpClient:HttpClient){}
 
   onNameKeyUp(event:any) {
-  this.destino = event.target.value;
-  console.log(this.destino)
-  this.httpClient.get(this.configServicios.destinos + this.slash + this.destino)
+    var c = this;
+    c.destino = event.target.value;
+    if(event.target.dataset.last != c.destino){
+      event.target.dataset.last = c.destino;
+      clearTimeout(c.timerid);
+      c.timerid = setTimeout(function() {
+        //$('#buscarColegio').addClass('loading');
+        c.httpClient.get(c.configServicios.destinosTemp + c.destino)
         .subscribe(
           (data:any[]) => {
-            console.log(data)
+            c.dataDestinos = data;
           }
         )
-    this.destino = event.target.value;
-    if(event.target.dataset.last != this.destino){
-      event.target.dataset.last = this.destino;
-      clearTimeout(this.timerid);
-      this.timerid = setTimeout(function() {
-        //$('#buscarColegio').addClass('loading');
-        /*httpClient.get(this.configServicios.destinos + this.slash + this.destino)
-        .subscribe(
-          (data:any[]) => {
-            console.log(data)
-          }
-        )*/
       },500);
     };
   }
-  
 
-  
-  //buscarDestinos('Lim');
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      /*map(term => term.length < 2 ? []
+        : this.dataDestinos.filter(v => v.pais.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))*/
+      map(term => term.length < 2 ? []
+        : misDestinos.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  )
+
+/*  formatter = (x: {
+    pais: string
+  }) => x.pais;*/
+
 }
--- reto-frontend/src/app/app.module.ts
+++ reto-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgbModule,
     FormsModule
   ],
   providers: [],
